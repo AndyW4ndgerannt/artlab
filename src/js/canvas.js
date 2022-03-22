293ebb62665d4f19b55fc26f88ac166b83bbf108
AndyW4ndgerannt <92064035+AndyW4ndgerannt@users.noreply.github.com>
2022-03-22 08:04:46 +0100

    Update canvas.js

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,1 +1,22 @@
-void ctx.scale(x, y);
+const imageBitmapPromise = createImageBitmap(image[, options]);
+const imageBitmapPromise = createImageBitmap(image, sx, sy, sw, sh[, options]);
+
+var canvas = document.getElementById('myCanvas'),
+ctx = canvas.getContext('2d'),
+image = new Image();
+
+// Wait for the sprite sheet to load
+image.onload = function() {
+  Promise.all([
+    // Cut out two sprites from the sprite sheet
+    createImageBitmap(image, 0, 0, 32, 32),
+    createImageBitmap(image, 32, 0, 32, 32)
+  ]).then(function(sprites) {
+    // Draw each sprite onto the canvas
+    ctx.drawImage(sprites[0], 0, 0);
+    ctx.drawImage(sprites[1], 32, 32);
+  });
+}
+
+// Load the sprite sheet from an image file
+image.src = 'https://github.com/AndyW4ndgerannt/artlab/blob/main/images/anima.png';
